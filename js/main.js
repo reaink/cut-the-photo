3a6d7b4e5a89103fde2e7ab2f6a4972e15a71d5b
不书 <nmlixa@163.com>
2019-02-14 14:16:27 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,44 @@
+layui.use(['element', 'layer'], function () {
+  var $ = layui.$,
+    layer = layui.layer;
+
+  var img = new Image();
+
+  $('#upload-img').on('change', uploadImg);
+
+  function uploadImg() {
+
+    var file = $(this).get(0).files[0],
+      contentbox = $('.main-content'),
+      imageType = /images*/;
+    
+    if (!file.type.match(imageType)) {
+      layer.msg('请选择一张图片！');
+      return;
+    } else {
+      var reader = new FileReader();
+
+      reader.onload = function () {
+        var img = new Image();
+        img.src = reader.result;
+
+        setTimeout(function () {
+          $(img).width = img.width + 'px';
+          $(img).height = img.height + 'px';
+
+          console.log(img.height);
+          contentbox.css({
+            width: '100%',
+            height: img.height + 'px',
+            background: 'url(' + reader.result + ') no-repeat top center / 100% auto',
+          })
+        })
+      };
+
+      reader.readAsDataURL(file);
+      
+
+    }
+    
+  }
+});
