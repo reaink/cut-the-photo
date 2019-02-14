1275c4513472073dba28e8b74b775e40ec169c6b
不书 <nmlixa@163.com>
2019-02-14 17:50:09 +0800

    添加：线连成区域

--- js/main.js
+++ js/main.js
@@ -4,31 +4,57 @@
 */
 layui.use(['element', 'layer'], function () {
   var $ = layui.$,
-    layer = layui.layer;
+    layer = layui.layer,
+    evClient;
+
 
   var contentBox = $('.main-content'),
-    _line = _createLineDom();
-  
+    _line = _createLineDom('#00dffc', true),
+    _oldLine,
+    removeBtn = document.createElement('div');
 
-  $('#upload-img').on('change', uploadImg);
+  function initElement() {
 
-  contentBox.on('mouseenter', function () {
-    $(contentBox).append(_line);
-  });
-  contentBox.on('mouseleave', function () {
-    $(_line).remove();
-  });
+    $(removeBtn).css({
+      display: 'none',
+      position: 'absolute',
+      'border-radius': '50%',
+      width: '30px',
+      height: '30px',
+      background: '#ccc',
+      color: 'red'
+    }).val('X')
 
-  contentBox.on('mousemove', contMove)
+    contentBox.append(removeBtn);
 
-  contentBox.on('mousemove', contMove)
+    $('#upload-img').on('change', uploadImg);
 
-  contentBox.on('click', addLine)
-  
+    contentBox.on('mouseenter', function () {
+      $(contentBox).append(_line);
+    });
+    contentBox.on('mouseleave', function () {
+      $(_line).remove();
+    });
 
-  function _createLineDom(bgcolor) {
+    contentBox.on('mousemove', contMove)
+
+    contentBox.on('mousemove', contMove)
+
+    $(window).on('scroll', function () {
+      contMove();
+    })
+
+    contentBox.on('click', addLine)
+
+    $('#split-btn').on('click', exportImg)
+  }
+
+  initElement();
+
+ 
+
+  function _createLineDom(bgcolor, isglobal) {
     var line = document.createElement('div');
-
     bgcolor = bgcolor || '#00dffc'
 
     $(line).css({
@@ -38,7 +64,48 @@ layui.use(['element', 'layer'], function () {
       background: bgcolor
     })
 
+    $(line).attr('class', 'line');
+
+    if (!isglobal) {
+      $(line).on('mouseover', function (ev) {
+        ev = ev || event;
+
+        $(removeBtn).css({
+          top: parseInt($(this).css('top')) - (parseInt($(removeBtn).css('width')) / 2) + 'px',
+          left: ev.clientX - parseInt(contentBox.offset().left) - (parseInt($(removeBtn).css('width')) / 2) + 'px',
+          display: 'inline-block',
+          zIndex: 999991
+        })
+
+        $(removeBtn).on('mouseenter', function () {
+          $(removeBtn).show();
+        }).on('mouseout', function () {
+          setTimeout(function () {
+            $(removeBtn).hide();
+          }, 1000);
+        }).on('click', function () {
+          $(line).remove();
+          return false;
+        })
+
+      }).on('mouseout', function () {
+        $(removeBtn).hide();
+      })
+    }
+
     return line;
+  }
+  function _creMask() {
+    var oDiv = document.createElement('div');
+
+    $(oDiv).css({
+      display: 'inline-block',
+      width: '100%',
+      background: 'rgba(0,0,0,.8)',
+      border: '1px solid rgba(255,255,255,.8)'
+    })
+
+    return oDiv;
   }
 
   function uploadImg() {
@@ -82,6 +149,7 @@ layui.use(['element', 'layer'], function () {
 
   function contMove(ev) {
     ev = ev || event;
+    
     $(_line).css({
       top: ev.clientY - contentBox.offset().top + $(window).scrollTop() + 'px'
     })
@@ -94,10 +162,28 @@ layui.use(['element', 'layer'], function () {
 
     console.log('add line of top:', $(_line).css('top'));
 
+    if (_oldLine && parseInt($(_line).css('top')) > parseInt($(_oldLine).css('top'))) {
+      var _oDiv = _creMask();
+
+      $(_oDiv).css({
+        position: 'absolute',
+        top: parseInt($(_oldLine).css('top')) - 1 + 'px',
+        height: parseInt($(_line).css('top')) - parseInt($(_oldLine).css('top')) + 'px'
+      });
+
+      contentBox.append(_oDiv);
+
+    }
+
     $(line).css({
       top: $(_line).css('top')
     })
 
     contentBox.append(line);
+    _oldLine = line;
+  }
+
+  function exportImg() {
+
   }
 });
