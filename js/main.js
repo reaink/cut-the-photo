d168ca066c33d4d8285a490be0c202dc434539c2
不书 <nmlixa@163.com>
2019-02-14 15:24:42 +0800

    添加完善功能 添加分割线

--- js/main.js
+++ js/main.js
@@ -2,14 +2,44 @@ layui.use(['element', 'layer'], function () {
   var $ = layui.$,
     layer = layui.layer;
 
-  var img = new Image();
+  var contentBox = $('.main-content'),
+    _line = _createLineDom();
+  
 
   $('#upload-img').on('change', uploadImg);
 
-  function uploadImg() {
+  contentBox.on('mouseenter', function () {
+    $(contentBox).append(_line);
+  });
+  contentBox.on('mouseleave', function () {
+    $(_line).remove();
+  });
 
+  contentBox.on('mousemove', contMove)
+
+  contentBox.on('mousemove', contMove)
+
+  contentBox.on('click', addLine)
+  
+
+  function _createLineDom(bgcolor) {
+    var line = document.createElement('div');
+
+    bgcolor = bgcolor || '#00dffc'
+
+    $(line).css({
+      position: 'absolute',
+      width: '100%',
+      height: '1px',
+      background: bgcolor
+    })
+
+    return line;
+  }
+
+  function uploadImg() {
     var file = $(this).get(0).files[0],
-      contentbox = $('.main-content'),
+      contentbox = contentBox,
       imageType = /images*/;
     
     if (!file.type.match(imageType)) {
@@ -39,9 +69,29 @@ layui.use(['element', 'layer'], function () {
       };
 
       reader.readAsDataURL(file);
-      
-
     }
+    
+  }
+
+  function contMove(ev) {
+    ev = ev || event;
+    $(_line).css({
+      top: ev.clientY - contentBox.offset().top + $(window).scrollTop() + 'px'
+    })
+  }
+
+  function addLine(ev) {
+    ev = ev || event;
+
+    var line = _createLineDom('#00f');
+
+    console.log('add line of:', $(_line).css('top'));
+
+    $(line).css({
+      top: $(_line).css('top')
+    })
+
+    contentBox.append(line);
     
   }
 });
